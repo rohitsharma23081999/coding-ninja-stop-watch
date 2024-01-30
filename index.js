a89c77b2ce1d93f8bfbf5748ee76e1edf46338f9
Rohit kumar <113030334+rohitsharma23081999@users.noreply.github.com>
2024-01-30 12:41:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+let hour = 0;
+let minute = 0;
+let second = 0;
+let milisecond = 0;
+// ---------------------------Code Main functionality--------------------------
+let timer = false;
+
+function start() {
+  timer = true;
+  myFAn();
+}
+function myFAn() {
+  if (timer == true) {
+    milisecond++;
+    if (milisecond == 100) {
+      second++;
+      milisecond = 0;
+    }
+    if (second == 60) {
+      minute++;
+      second = 0;
+    }
+    if (minute == 60) {
+      hour++;
+      minute = 0;
+    }
+    // -------------------------
+    let getHour = hour;
+    let getMinute = minute;
+    let getSecond = second;
+    if (second < 10) {
+      getSecond = "0" + second;
+    }
+    if (minute < 10) {
+      getMinute = "0" + minute;
+    }
+    if (hour < 10) {
+      getHour = "0" + hour;
+    }
+    setTimeout("myFAn()", 10);
+
+    document.getElementById("mili").textContent = milisecond;
+    document.getElementById("second").textContent = getSecond;
+    document.getElementById("minute").textContent = getMinute;
+    document.getElementById("hour").textContent = getHour;
+  }
+}
+
+// ------------------------------code stop function----------------------
+function stop() {
+  timer = false;
+  myFAn();
+  return;
+}
+// ---------------------code reset function----------------------------
+function reset() {
+  location.reload();
+  return;
+}
